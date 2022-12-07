08f5d3c710c768c5d47b6bbe497b311130d6f1cc
wii <zhenkai.sun@qq.com>
2022-12-08 00:27:01 +0800

    fix

--- openssl/install.cmake
+++ openssl/install.cmake
@@ -4,15 +4,14 @@ include(${CMAKE_CURRENT_LIST_DIR}/../base.cmake)
 function(Process)
     get_filename_component(DEP_NAME ${CMAKE_CURRENT_LIST_DIR} NAME)
     SetBasicVariables(NAME ${DEP_NAME} VERSION OpenSSL_1_1_1s)
-    # TODO 安装位置不对
     ExternalProject_Add(
             ${DEP_NAME}
             PREFIX ${DEPS_PREFIX}
             URL ${GITHUB_DOWNLOAD_URL}
             INSTALL_DIR ${DEP_INSTALL_DIR}
             CONFIGURE_COMMAND <SOURCE_DIR>/config --prefix=${DEP_INSTALL_DIR}
-            BUILD_COMMAND ${MAKE_EXECUTABLE} -j -C <SOURCE_DIR>
-            INSTALL_COMMAND ${MAKE_EXECUTABLE} -j -C <SOURCE_DIR> install PREFIX=<INSTALL_DIR> prefix=<INSTALL_DIR>
+            BUILD_COMMAND ${MAKE_EXECUTABLE} -j
+            INSTALL_COMMAND ${MAKE_EXECUTABLE} -j install PREFIX=<INSTALL_DIR> prefix=<INSTALL_DIR>
     )
 endfunction(Process)
 Process()
