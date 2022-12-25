74d15c96a7d65d8d44c72cdceaf36301581585f8
wii <zhenkai.sun@qq.com>
2022-12-26 00:29:43 +0800

    stage

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ```shell
 cmake >= 3.20
 ```
+
+# Install
+```shell
+# ubuntu
+sudo apt install gcc g++ make automake autoconf libtool
+```
--- gcc/load.cmake
+++ gcc/load.cmake
@@ -6,7 +6,7 @@ if (BUILD_GCC)
             VERSION releases/gcc-10.4.0
             AUTHOR gcc-mirror
             BUILD_IN_SOURCE TRUE
-            CONFIGURE_COMMAND ./contrib/download_prerequisites && ./configure --prefix=<INSTALL_DIR> --enable-languages=c,c++ --disable-multilib
+            CONFIGURE_COMMAND ./contrib/download_prerequisites && ./configure --prefix=<INSTALL_DIR> --enable-languages=c,c++
             BUILD_COMMAND ${MAKE_EXECUTABLE} -j -C <SOURCE_DIR>
             INSTALL_COMMAND ${MAKE_EXECUTABLE} -j -C <SOURCE_DIR> install
     )
